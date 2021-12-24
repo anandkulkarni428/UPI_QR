63dc10edaa5ce51504eb38841b4b2475fc888051
anandkulkarni428 <57906010+anandkulkarni428@users.noreply.github.com>
2021-12-25 00:04:17 +0530

    Ui modification across the application

--- app/src/main/java/com/anand/upiqr/Activity/MainActivity.java
+++ app/src/main/java/com/anand/upiqr/Activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.anand.upiqr.R;
@@ -20,7 +21,7 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    TextInputEditText amountTxt, upiIdTxt, firstNameTxt, lastNameTxt;
+    EditText amountTxt, upiIdTxt, firstNameTxt, lastNameTxt;
     Button qrBtn;
     ActivityMainBinding binding;
 
